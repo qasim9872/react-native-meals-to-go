98f979806239bb5628db7bb3937886bb32dd0d77
Muhammad Qasim <qasim9872@gmail.com>
2021-05-31 18:28:44 +0100

    added default layout

--- App.js
+++ App.js
@@ -1,8 +1,53 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+
+import { spacing } from "./src/constants/sizes";
+
+const isAndroid = Platform.OS === "android";
 
 export default function App() {
-  return <View></View>;
+  return (
+    <>
+      <SafeAreaView style={styles.safeView}>
+        <View style={styles.appContainer}>
+          <View style={styles.searchBar}>
+            <Text>Search bar</Text>
+          </View>
+
+          <View style={styles.appView}>
+            <Text>App</Text>
+          </View>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  safeView: {
+    flex: 1,
+    marginTop: isAndroid ? StatusBar.currentHeight : 0,
+  },
+  appContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  searchBar: {
+    padding: spacing.md,
+    backgroundColor: "green",
+  },
+  appView: {
+    flex: 1,
+    padding: spacing.md,
+    backgroundColor: "yellow",
+  },
+});
