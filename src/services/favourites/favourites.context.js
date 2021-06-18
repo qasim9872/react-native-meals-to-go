cb0130b8f0e5ed7b155d9288718e4b9b547ba217
Muhammad Qasim <qasim9872@gmail.com>
2021-06-18 20:02:34 +0100

    saving favourites to async storage

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,9 +1,31 @@
-import React, { useState, createContext, useContext } from "react";
+import React, { useState, createContext, useContext, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavouritesContext = createContext();
+const StorageTag = "MealsToGo:Favourites";
 
 export const FavouritesContextProvider = ({ children }) => {
   const [favourites, setFavourites] = useState([]);
+
+  const saveFavourites = (value) =>
+    AsyncStorage.setItem(StorageTag, JSON.stringify(value)).catch(
+      console.error
+    );
+
+  const loadFavourites = async () => {
+    const value = await AsyncStorage.getItem(StorageTag).catch(console.error);
+    if (value) {
+      setFavourites(JSON.parse(value));
+    }
+  };
+
+  useEffect(() => {
+    loadFavourites();
+  }, []);
+
+  useEffect(() => {
+    saveFavourites(favourites);
+  }, [favourites]);
 
   const addToFavourites = (restaurant) =>
     setFavourites([...favourites, restaurant]);
