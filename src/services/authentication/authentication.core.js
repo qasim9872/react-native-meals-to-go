063a1ecc3535bd51a0bab7c789172788c4ab24ab
Muhammad Qasim <qasim9872@gmail.com>
2021-06-20 16:52:53 +0100

    refactoring firebase logic

---
+++ src/services/authentication/authentication.core.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAAUZ0-h_Kvb-T-VaPG2gDwarLdb6nhqTo",
+  authDomain: "meals-to-go-f1537.firebaseapp.com",
+  projectId: "meals-to-go-f1537",
+  storageBucket: "meals-to-go-f1537.appspot.com",
+  messagingSenderId: "140085611465",
+  appId: "1:140085611465:web:b040ab438adf6641a4f618",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
