2ce1e5e372afef44b994bd3c0a7fbd5619e84039
Muhammad Qasim <qasim9872@gmail.com>
2021-06-21 20:50:54 +0100

    updated settings screen

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,24 +1,40 @@
 import React from "react";
-import { List } from "react-native-paper";
+import { Avatar, List } from "react-native-paper";
+import styled from "styled-components/native";
+
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { Text } from "../../../components/typography/text.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { useAuthenticationContext } from "../../../services/authentication/authentication.context";
 
+const SettingsItem = styled(List.Item)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const AvatarContainer = styled.View`
+  align-items: center;
+`;
+
 export const SettingsScreen = ({ navigation: { navigate } }) => {
-  const { onLogout } = useAuthenticationContext();
+  const { onLogout, user } = useAuthenticationContext();
 
   return (
     <SafeArea>
+      <AvatarContainer>
+        <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+        <Spacer size="large" position="top">
+          <Text variant="label">{user.email}</Text>
+        </Spacer>
+      </AvatarContainer>
       <List.Section>
-        <List.Item
-          style={{ padding: 16 }}
+        <SettingsItem
           title="Favourites"
           description="View your favourites"
           left={(props) => <List.Icon {...props} color="black" icon="heart" />}
           onPress={() => navigate("Favourites")}
         />
 
-        <List.Item
-          style={{ padding: 16 }}
+        <SettingsItem
           title="Logout"
           left={(props) => <List.Icon {...props} color="black" icon="door" />}
           onPress={onLogout}
