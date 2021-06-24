66e4879bbf3faf48b99f4181daffad0c6f5cc7f3
Muhammad Qasim <qasim9872@gmail.com>
2021-06-24 14:15:36 +0100

    favourites screen

--- src/features/settings/screens/favourites.screen.js
+++ src/features/settings/screens/favourites.screen.js
@@ -1,11 +1,29 @@
 import React from "react";
-import { Text } from "react-native";
+import styled from "styled-components/native";
+
+import { RestaurantList } from "../../../components/restaurant/RestaurantList.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
+import { Text } from "../../../components/typography/text.component";
+import { useFavouritesContext } from "../../../services/favourites/favourites.context";
+
+const NoFavouritesArea = styled(SafeArea)`
+  justify-content: center;
+  align-items: center;
+`;
 
 export const FavouritesScreen = ({ navigation: { navigate } }) => {
-  return (
+  const { favourites } = useFavouritesContext();
+
+  return favourites && favourites.length ? (
     <SafeArea>
-      <Text>Favourite Screen</Text>
+      <RestaurantList
+        restaurants={favourites}
+        onClick={(restaurant) => navigate("RestaurantDetails", restaurant)}
+      />
     </SafeArea>
+  ) : (
+    <NoFavouritesArea>
+      <Text>No favourites Yet</Text>
+    </NoFavouritesArea>
   );
 };
